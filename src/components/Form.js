ccbecf0b86c0fb594164274942f348364be18fcf
alex-merecka <amerecka@webcreek.com>
2022-02-14 15:59:18 -0600

    FInished lesson

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,14 +1,23 @@
 import React, { useState } from "react";
 
 function Form() {
-  const [firstName, setFirstName] = useState("John");
-  const [lastName, setLastName] = useState("Henry");
+  const [number, setNumber] = useState(0);
+  const [error, setError] = useState(null);
+
+  function handleNumberChange(event) {
+    const newNumber = parseInt(event.target.value);
+    if (newNumber >= 0 && newNumber <= 5) {
+      setNumber(newNumber);
+      setError(null);
+    } else {
+      setError(`${newNumber} is not a valid number!`);
+    }
+  }
 
   return (
     <form>
-      <input type="text" value={firstName} />
-      <input type="text" value={lastName} />
-      <button type="submit">Submit</button>
+      <input type="number" value={number} onChange={handleNumberChange} />
+      {error ? <span style={{ color: "red" }}>{error}</span> : null}
     </form>
   );
 }
